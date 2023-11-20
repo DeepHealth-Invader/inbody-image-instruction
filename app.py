58a3a8f6f7a8bd718433229011aeff4f06bbff17
Emsoohyeok <dkdl8523@naver.com>
2023-11-20 20:57:12 +0900

    엠수프론투

--- app.py
+++ app.py
@@ -1,22 +1,83 @@
 import streamlit as st
+import os
+
 
 # 페이지 기본 설정
 st.set_page_config(
-    page_icon="👻",
-    page_title="인바디 이미지 분석",
+    page_icon="🦾",
+    page_title="GPT쌤의 PT 상담소",
     layout="wide",
 )
 
+
+# 스타일링 CSS 정의
+header_style = """
+    color:  #00FF00; 
+    text-align: center;
+    font-size: 36px;
+    font-weight: bold;
+    margin-bottom: 20px;
+"""
+
+subheader_style = """
+    color: #4a4a4a;
+    text-align: center;
+    font-size: 24px;
+    margin-bottom: 20px;
+"""
+
+text_style = """
+    color: #4a4a4a;
+    text-align: left;
+    font-size: 16px;
+"""
+
 # 페이지 헤더, 서브헤더 제목 설정
-st.title("인바디 이미지 분석 사이트")
+st.markdown("<h1 style='{}'>어서오세요~GPT GYM 입니다 🎉".format(header_style), unsafe_allow_html=True)
 
-st.header("서비스 소개")
-st.text("안녕하십니까. DeepHealth-Invadors 팀 입니다. 저희의 캡스톤 디자인 2의 주제는 '인바디 이미지를 분석해 건강을 지도해주는 애플리케이션 제작' 입니다.")
 
-st.header("기술 스택 & 팀원 소개")
-st.text("임수혁 : 딥러닝 분류 모델 설계") 
-st.text("김의찬 :  웹 파트 1") 
-st.text("민선익 :  웹 파트 2")
+st.markdown(
+    """
+    ## GPT 쌤의 유쾌한 PT 상담소 🏋️‍♂️💬
+
+    안녕하세요! GPT 쌤 여기 상담소에 오신 여러분을 환영합니다! 🎉
+
+    여기서는 인바디 이미지를 분석하여 건강을 지도해주는 애플리케이션을 제작하는 DeepHealth-Invadors 팀의 멋진 프로젝트를 소개합니다. 😊
+
+    제가 여러분의 PT 쌤으로 함께 건강을 책임지고 즐겁게 지도해드릴게요! 🌟
+
+    이미지를 업로드하고, 팀원들과 함께 여러 기술을 활용하여 건강한 삶을 위한 팁을 제공합니다. 건강한 몸과 마음으로 여러분을 기다리고 있어요! 💪💖
+
+    왼쪽의 inbody 탭을 눌러 인바디 사진을 업로드만 하면 끝나니까 얼른 해보세요😊!
+    """
+)
+
+st.markdown("<h2 style='{}'>개발진 소개</h2>".format(subheader_style), unsafe_allow_html=True)
+
+# 이미지 파일들이 있는 디렉토리 경로
+image_dir = "C:/Users/user-pc/PycharmProjects/inbody-image-instruction/image"
+
+# 이미지 파일들의 이름 리스트
+image_names = ["임수혁.png", "김의찬.png", "민선익.png"]
+
+# 각 이미지를 스트림릿에 표시 및 역할 추가
+col1, col2, col3 = st.columns(3)
+
+for i, image_name in enumerate(image_names):
+    with eval(f"col{i+1}"):
+        # 각 이미지를 스트림릿에 표시
+        image_path = os.path.join(image_dir, image_name)
+        image = open(image_path, "rb").read()
+        st.image(image, caption=None, use_column_width=True)
+
+        # 팀원 역할 텍스트 추가
+        st.markdown("<h3 style='{}'>{}</h3>".format(subheader_style, image_name.replace('.png', '')), unsafe_allow_html=True)
+        if i == 0:
+            st.markdown("<p style='{}'>🎨 프론트엔드 디자인과 구현<br>🖥 사용자 인터페이스(UI) 담당</p>".format(text_style), unsafe_allow_html=True)
+        elif i == 1:
+            st.markdown("<p style='{}'>🚀 이미지 처리 및 결과 표시<br>🧠 이미지 검출 모델 설계 및 구현 담당</p>".format(text_style), unsafe_allow_html=True)
+        elif i == 2:
+            st.markdown("<p style='{}'>🛠 백엔드 시스템 아키텍처 설계 및 구현<br>🌐 외부 API와의 통합 담당</p>".format(text_style), unsafe_allow_html=True)
 
 
 
--- pages/inbody.py
+++ pages/inbody.py
@@ -1,4 +1,3 @@
-import openai
 import streamlit as st
 import cv2
 import numpy as np
@@ -6,17 +5,21 @@ import pytesseract
 from pytesseract import Output
 import re
 from openai import OpenAI
-from config import OPENAI_API_KEY
-
+from pages.config import OPENAI_API_KEY
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon="🦾",
+    page_title="GPT쌤의 PT 상담소",
+    layout="wide",
+)
 
 
 st.markdown("""
 <style>
-body {
-    background-color: #f4f4f4;
-}
+
+
 .big-font {
-    font-size:50px !important;
+    font-size: 50px !important;
     color: #5a9;
     text-align: center;
     padding-top: 50px;
@@ -25,6 +28,17 @@ body {
     display: flex;
     justify-content: center;
     padding: 50px;
+}
+.button {
+    background-color: #5a9;
+    color: white;
+    padding: 10px 20px;
+    font-size: 16px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.button:hover {
+    background-color: #3c6;
 }
 .result-header {
     color: #5a9;
@@ -38,44 +52,46 @@ body {
     font-size: 20px;
     text-align: center;
 }
+.waiting {
+    text-align: center;
+    font-size: 24px;
+    margin-top: 20px;
+}
 </style>
 """, unsafe_allow_html=True)
 
 st.markdown("""
 <p>
     <div class="big-font">
-        인바디 사진 분석기
+        회원 상담실 🦾
     </div>
 </p>    
 """, unsafe_allow_html=True)
 
 st.markdown("""
 <div class="description">
-    <p>이 애플리케이션은 인바디 사진을 분석하여 체중, 골격근량, 체지방량을 알려줍니다.<br /> 
-    사진을 업로드하면 결과를 확인할 수 있습니다.</p> 
+    <p>안녕하세요! GPT 쌤이에요~ 🌟<br />
+    여러분의 인바디 사진을 주시면,<br />
+    체중, 골격근량, 체지방량을 멋지게 분석해드려요! 📸✨<br />
+    사진 업로드 하시면 분석 들어갈께요 ~🔄💻.</p> 
 </div>
 """, unsafe_allow_html=True)
 
-
-uploaded_file = st.file_uploader("인바디 사진을 업로드하세요", type=["jpg", "png"], key="1")
+uploaded_file = st.file_uploader("네,회원님 여기에다가 업로드 부탁드릴께요~😊", type=["jpg", "png"], key="1")
 
 if uploaded_file is not None:
-    # 이미지를 OpenCV 이미지로 변환
     img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-
-    # 이미지를 가로로 3등분하기
     height, width, _ = img.shape
     part_height = height // 3
     img_parts = [img[part_height * i:part_height * (i + 1), :] for i in range(3)]
 
-    # 각 부분에서 소수점 첫째자리까지 표기된 숫자 찾기
     weights, muscles, fats = [], [], []
     for i, img_part in enumerate(img_parts):
         data = pytesseract.image_to_data(img_part, lang='eng', output_type=Output.DICT)
         numbers = [(float(num), data['height'][j]) for j, num in
                    enumerate(re.findall('\d+\.\d', ' '.join(data['text']))) if num]
         if numbers:
-            num, _ = max(numbers, key=lambda x: x[1])  # 폰트 크기가 가장 큰 숫자 선택
+            num, _ = max(numbers, key=lambda x: x[1])
             if i == 0:
                 weights.append(num)
             elif i == 1:
@@ -83,24 +99,20 @@ if uploaded_file is not None:
             else:
                 fats.append(num)
 
-    # 각 부분에서 폰트 크기가 가장 큰 숫자를 변수에 저장
     weight = max(weights) if weights else None
     muscle = max(muscles) if muscles else None
     fat = max(fats) if fats else None
 
-    # 결과 출력
-    st.markdown("<h2 class='result-header'>결과</h2>", unsafe_allow_html=True)
-    st.markdown(f"<p class='result-text'>체중: {weight}</p>", unsafe_allow_html=True)
-    st.markdown(f"<p class='result-text'>골격근량: {muscle}</p>", unsafe_allow_html=True)
-    st.markdown(f"<p class='result-text'>체지방량: {fat}</p>", unsafe_allow_html=True)
+    st.markdown("<h2 class='result-header'>회원님</h2>", unsafe_allow_html=True)
+    st.markdown(f"<p class='result-text'>체중: {weight} kg 이시고</p>", unsafe_allow_html=True)
+    st.markdown(f"<p class='result-text'>골격근량: {muscle} 이렇게 되시고 ..</p>", unsafe_allow_html=True)
+    st.markdown(f"<p class='result-text'>체지방량: {fat} 이시네요 !</p>", unsafe_allow_html=True)
 
-    # 이미지 출력
     st.markdown("<h2 class='result-header'>분석한 이미지 부분</h2>", unsafe_allow_html=True)
     for i, img_part in enumerate(img_parts):
         st.image(img_part, channels="BGR", use_column_width=True)
 
-    # 생성중
-    st.markdown("<h2 class='result-header'>GPT 응답중 ... </h2>", unsafe_allow_html=True)
+    st.markdown("<p class='result-header'>제가 직접 하나하나 세세하게 적어드릴테니까 회원님 잠시만 기다려주세요 ~ ! 😉  </p>", unsafe_allow_html=True)
 
     # GPT API KEY 정보
     client = OpenAI(api_key=OPENAI_API_KEY)
@@ -109,11 +121,30 @@ if uploaded_file is not None:
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"체중: {weight}, 골격근량: {muscle}, 체지방량: {fat}. 건강 지도를 알려주세요."}
+            {"role": "system", "content": "You are a personal trainer."},
+            {"role": "user", "content": f"체중: {weight}, 골격근량: {muscle}, 체지방량: {fat}. 이렇게 되는데 어떻게 운동 방식을 가져가야하는지와 식단조절 및 방법을 회원님께 설명해줘."}
         ]
     )
 
-    # GPT API 응답 출력
-    st.markdown("<h2 class='result-header'>GPT 응답 결과</h2>", unsafe_allow_html=True)
-    st.markdown(completion.choices[0].message.content)
+    # Waiting for GPT response animation
+    st.markdown("<div class='waiting'>GPT썜 적는 중 ...</div>", unsafe_allow_html=True)
+
+    # GPT 응답 결과 텍스트
+    gpt_response = completion.choices[0].message.content
+
+    # 결과 출력
+    st.markdown("<h2 class='result-header'>자 회원님 한번 같이 봐보실까요 ?💻 </h2>", unsafe_allow_html=True)
+    st.markdown(gpt_response)
+
+    # 결과를 파일로 저장
+    with st.expander("회원님 지인 소개시 10% 할인 들어가시는데 어떻게 하시겠어요 ?🦾😊 "):
+        # 결과를 파일로 저장
+        result_file_path = "gpt_response.txt"
+        with open(result_file_path, "w", encoding="utf-8") as result_file:
+            result_file.write(gpt_response)
+
+        # 결과 파일 다운로드 링크 생성
+        st.markdown(
+            f"[**결과 다운로드**](sandbox:/view/{result_file_path})",
+            unsafe_allow_html=True
+        )
